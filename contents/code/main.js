3d035a2135dd696186cf8b30a0a31ffb88daf8b7
Joe D <joedef@gmail.com>
2021-12-01 10:43:34 -0500

    Numerous fixes; remaining issue is the mouse does not follow moved window.

--- contents/code/main.js
+++ contents/code/main.js
@@ -4,14 +4,35 @@ KWin Script Move Win Directionally
 GNU General Public License v3.0
 */
 
-const DB = true;
-function db(...args) {if (DB) {console.debug(...args);}}
-db("initializing Move Win Directionally");
+const DB = false;
+if (DB) print("initializing Move Win Directionally");
 
+function calc_overlap(geo1, geo2) {
+    // Caculate the overlapping area of two geometries.
+    var tlx = Math.max(geo1.x, geo2.x);
+    var tly = Math.max(geo1.y, geo2.y);
+    var brx = Math.min(geo1.x + geo1.width, geo2.x + geo2.width);
+    var bry = Math.min(geo1.y + geo1.height, geo2.y + geo2.height);
+    var overlap = Math.max(0, brx - tlx) * Math.max(0, bry - tly);
+    return overlap;
+}
+
+function calc_distance(geo1, geo2) {
+    // calculate the distance between the centers of two geometries
+    delta_x = (geo2.x + geo2.width/2) - (geo1.x + geo1.width/2);
+    delta_y = (geo2.y + geo2.height/2) - (geo1.y + geo1.height/2);
+    return Math.sqrt(delta_x*delta_x + delta_y*delta_y);
+}
+    
 function screen_to_the(direction, current_screen) {
-    // shift the current screen in the given direction and compute screen_to_the
-    // overlap with all other screens to find the one with the most.
-    // If returns the best overlapping screen if any; otherwise returns the current screen.
+    // Find the screen "most" in the given direction from the current screen by:
+    //  - shift the current screen in the given direction
+    //  - for all screeens, compute the overlap with the shifted screen
+    //  - choose/return the screen with the most overlap (if no overlaps,
+    //    return the current screen).
+    // NOTE: there are some degenerate cases where this might not work; e.g.,
+    // from left to right, a huge screen, a tiny screen, and another huge screen.
+    // If that ever becomes an issue, the a little more work is needed.
     const screen_geo = workspace.clientArea(KWin.MaximizeArea, current_screen, workspace.currentDesktop);
     // const shifted_geo = Object.assign({}, screen_geo);
     const shifted_geo = JSON.parse(JSON.stringify(screen_geo));
@@ -25,51 +46,83 @@ function screen_to_the(direction, current_screen) {
     } else if (direction == "up") {
         shifted_geo.y -= shifted_geo.height;
     }
-    var best_overlap = 0;
-    var best_screen = current_screen;
+    var 
+    most_overlap = 0;
+    var most_screen = current_screen;
     for (var i = 0; i < nscreens; i++) {
         var geo = workspace.clientArea(KWin.MaximizeArea, i, workspace.currentDesktop);
-        var tlx = Math.max(shifted_geo.x, geo.x);
-        var tly = Math.max(shifted_geo.y, geo.y);
-        var brx = Math.min(shifted_geo.x + shifted_geo.width, geo.x + geo.width);
-        var bry = Math.min(shifted_geo.y + shifted_geo.height, geo.y + geo.height);
-        var overlap = Math.max(0, brx-tlx) * Math.max(0, bry - tly);
-        if (overlap > best_overlap) {
-            best_overlap = overlap;
-            best_screen = i;
+        var overlap = calc_overlap(shifted_geo, geo);
+        if (overlap > most_overlap) {
+            most_overlap = overlap;
+            most_screen = i;
         }
     }
-    return best_screen;
+    return most_screen;
+}
+
+function which_screen(client_geo) {
+    // find the screen that the client is most nearly on.
+    // if overlapping, choose the biggest; if no overlap, choose closest.
+    var most_overlap = 0;
+    var most_screen = 0;
+    var closest_distance = 2000*1000;
+    var closest_screen = 0;
+    for (var i = 0; i < nscreens; i++) {
+        var geo = workspace.clientArea(KWin.MaximizeArea, i, workspace.currentDesktop);
+        var overlap = calc_overlap(client_geo, geo);
+        if (overlap > most_overlap) {
+            most_overlap = overlap;
+            most_screen = i;
+        }
+        var distance = calc_distance(client_geo, geo);
+        if (distance < closest_distance) {
+            closest_distance = distance;
+            closest_screen = i;
+        }
+    }
+    if (DB) print('most area/screen:', most_overlap, most_screen);
+    if (most_overlap > 0) return most_screen;
+    if (DB) print('closest distance/screen:', closest_distance, closest_screen);
+    return closest_screen;
 }
 
 if (DB) {
     var client = workspace.activeClient;
     var nscreens = workspace.numScreens;
-    db("nscreens", nscreens)
+    print("nscreens", nscreens)
     for (var i = 0; i < nscreens; i++) {
         var geo = workspace.clientArea(KWin.MaximizeArea, i, workspace.currentDesktop);
-        db(i, JSON.stringify(geo), "screen_to_the", "right", screen_to_the("right", i));
-        db(i, JSON.stringify(geo), "screen_to_the", "left", screen_to_the("left", i));
-        db(i, JSON.stringify(geo), "screen_to_the", "up", screen_to_the("up", i));
-        db(i, JSON.stringify(geo), "screen_to_the", "down", screen_to_the("down", i));
+        print(i, JSON.stringify(geo), "screen_to_the", "right", screen_to_the("right", i));
+        print(i, JSON.stringify(geo), "screen_to_the", "left", screen_to_the("left", i));
+        print(i, JSON.stringify(geo), "screen_to_the", "up", screen_to_the("up", i));
+        print(i, JSON.stringify(geo), "screen_to_the", "down", screen_to_the("down", i));
     }
+    client_screen = which_screen(client.geometry);
+    print("client_screen:", client_screen);
 }
 
 function move_win(direction) {
-    if (DB) db("move_win(", direction, ")");
+    if (DB) print("move_win(", direction, ")");
     var client = workspace.activeClient;
     if (client == null || !client.normalWindow) return;
-    new_screen = screen_to_the(direction);
+    new_screen = screen_to_the(direction, workspace.activeScreen);
+    if (DB) print('new_screen:', new_screen, "old_screen:", workspace.activeScreen);
     workspace.sendClientToScreen(client, new_screen);
     // clip and move client into bounds of screen dimensions
+    if (DB) print("client.moveable:", client.moveable, "ogeo:",
+        JSON.stringify(client.geometry));
     if (!client.moveable) return;
-    area = workspace.clientArea(KWin.WorkArea, client);
+    client_screen = which_screen(client.geometry);
+    area = workspace.clientArea(KWin.MaximizeArea, client_screen, workspace.currentDesktop);
+    if (DB) print("screen:", JSON.stringify(area));
     // window width/height maximally screen width/height
-    client.geometry.width = Math.min(client.width, area.width);
-    client.geometry.height = Math.min(client.height, area.height);
+    const width = Math.min(client.width, area.width);
+    const height = Math.min(client.height, area.height);
     // left/top window edge between left and right/top and bottom screen edges
-    client.geometry.x = Math.max(area.x, Math.min(area.x + area.width - client.width, client.x));
-    client.geometry.y = Math.max(area.y, Math.min(area.y + area.height - client.height, client.y));
+    const x = client.geometry.x = Math.max(area.x, Math.min(area.x + area.width - client.width, client.x));
+    const y = client.geometry.y = Math.max(area.y, Math.min(area.y + area.height - client.height, client.y));
+    client.geometry = {x: x, y: y, width: width, height: height};
+    if (DB) print("ngeo:", JSON.stringify(client.geometry));
 }
     
 function move_win_right() { move_win("right"); }
